ed99f2df0f490cc0a53b14f611847c9e998b0ee2
Mohamed Beshri Amer <mohamed.perry351@gmail.com>
2020-05-24 13:06:31 -0700

    Add Repositry Generic and DataTable Plugin

---
+++ GraduationProject/Repositry/IRepositry.cs
@@ -0,0 +1,28 @@
+﻿using GraduationProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraduationProject.Repositry
+{
+   public interface IRepositry<T,ID> where T:class
+    {
+        public IEnumerable<T> GetAll();
+
+        public T Get(ID Key);
+
+        public void Add(T entity);
+
+        public void Edit(T entity);
+
+        public void Delete(T entity);
+
+        public DataTableViewModel<T> GetDataTable(int start, int lenght, Func<T, bool> search, Func<T, ID> OrderBy);
+
+
+        public int SaveAll();
+        
+    }
+
+}
