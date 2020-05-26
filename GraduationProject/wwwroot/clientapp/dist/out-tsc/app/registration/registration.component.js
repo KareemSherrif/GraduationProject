1f41b210c1383fa3903d16b7bb413e64a85215ef
Mohamed Beshri Amer <mohamed.perry351@gmail.com>
2020-05-26 09:21:59 -0700

    Finish Registration Form

--- GraduationProject/wwwroot/clientapp/dist/out-tsc/app/registration/registration.component.js
+++ GraduationProject/wwwroot/clientapp/dist/out-tsc/app/registration/registration.component.js
@@ -24,6 +24,7 @@ let RegistrationComponent = class RegistrationComponent {
         if (data.value != 0) {
             this.areaService.GetAreas(data.value).subscribe(a => {
                 this.areas = a;
+                console.log(a);
             });
         }
         else {
