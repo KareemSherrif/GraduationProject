2acf7de8593ba9ec42d2eef1cf4937e3a7c1efba
shahende <shahenda.mena@gmail.com>
2020-05-31 12:31:58 +0200

    Add Product Controller and Repository

---
+++ GraduationProject/wwwroot/clientapp/dist/out-tsc/app/guards/auth-interceptor.js
@@ -0,0 +1,33 @@
+import { __decorate } from "tslib";
+import { Injectable } from "@angular/core";
+import { tap } from 'rxjs/operators';
+let AuthInterceptor = class AuthInterceptor {
+    constructor(router, userService) {
+        this.router = router;
+        this.userService = userService;
+    }
+    intercept(req, next) {
+        const userToken = this.userService.CurrentUserToken();
+        if (userToken != null) {
+            req = req.clone({
+                setHeaders: {
+                    Authorization: "Bearer " + userToken
+                }
+            });
+            return next.handle(req).pipe(tap(success => { }, err => {
+                if (err.status == 401) {
+                    localStorage.removeItem('token');
+                    this.router.navigateByUrl('/login');
+                }
+            }));
+        }
+        else {
+            return next.handle(req.clone());
+        }
+    }
+};
+AuthInterceptor = __decorate([
+    Injectable()
+], AuthInterceptor);
+export { AuthInterceptor };
+//# sourceMappingURL=auth-interceptor.js.map
---
+++ GraduationProject/wwwroot/clientapp/dist/out-tsc/app/registration/registration.component.js
@@ -0,0 +1,54 @@
+/** @format */
+import { __decorate } from "tslib";
+import { Component } from "@angular/core";
+let RegistrationComponent = class RegistrationComponent {
+    constructor(areaService, citiesServices, register, toastr) {
+        this.areaService = areaService;
+        this.citiesServices = citiesServices;
+        this.register = register;
+        this.toastr = toastr;
+        this.passwordConfirmation = false;
+    }
+    ngOnInit() {
+        this.citiesServices.GetCities().subscribe((a) => {
+            this.cities = a;
+        });
+    }
+    OnRegistration(data) {
+        if (data.valid) {
+            console.log(data.value);
+            this.register.RegisterUser(data.value).subscribe((a) => {
+                this.toastr.success("The Registration has been success");
+            }, (err) => {
+                console.log(err);
+                this.toastr.error(err.error);
+            });
+        }
+    }
+    OComparePasswords(password, confirmPassword) {
+        console.log(confirmPassword.value);
+        console.log(password.value);
+        this.passwordConfirmation = password.value != confirmPassword.value;
+        console.log(this.passwordConfirmation);
+    }
+    OnCityChange(data) {
+        if (data.value != 0) {
+            this.areaService.GetAreas(data.value).subscribe((a) => {
+                this.areas = a;
+                console.log(a);
+            });
+        }
+        else {
+            this.areas = [];
+        }
+    }
+};
+RegistrationComponent = __decorate([
+    Component({
+        selector: "app-registration",
+        templateUrl: "./registration.component.html",
+        styleUrls: ["./registration.component.css"],
+    })
+], RegistrationComponent);
+export { RegistrationComponent };
+//# sourceMappingURL=registration.component.js.map
